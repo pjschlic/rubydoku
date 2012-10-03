a02b07286a769d017e309b2b635f92bf630906cc
Peter Schlichting <pjschlic@gmail.com>
2012-10-03 16:20:14 -0700

    spacing

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -9,14 +9,14 @@ class Tile
     # 1-9 each represented by a bit
     @value_mask = 0x1ff
   end
-	def valid?
-		neighbors do |neighbor|
-			if neighbor.value == @value
-				return false
-			end
-		end
-		return true
-	end
+  def valid?
+    neighbors do |neighbor|
+      if neighbor.value == @value
+        return false
+      end
+    end
+    return true
+  end
   def set(puzzle,x,y)
     @puzzle=puzzle
     @x=x
@@ -32,17 +32,17 @@ class Tile
       puts "placing value #{value} at #{@x},#{@y}"
       @value=value
       self.mask(0x0)
-			neighbors do |neighbor|
-				neighbor.mask(~(1 << (@value-1)))
-			end
+      neighbors do |neighbor|
+        neighbor.mask(~(1 << (@value-1)))
+      end
       # mask_constraints(~(1 << (@value-1)))
     end
   end
-	def neighbors
-		yield_row {|tile| yield(tile)}
-		yield_col {|tile| yield(tile)}
-		yield_section {|tile| yield(tile)}
-	end
+  def neighbors
+    yield_row {|tile| yield(tile)}
+    yield_col {|tile| yield(tile)}
+    yield_section {|tile| yield(tile)}
+  end
   def mask(mask)
     if @value_mask == 0
       return
@@ -54,7 +54,7 @@ class Tile
       new_mask = @value_mask & mask
       if @value_mask == 0
         puts "ERROR INVALID PUZZLE: 0x#{mask.to_s(16)} being applied at (#{@x},#{@y}) which is #{@value_mask}"
-				raise
+        raise
       end
       if new_mask != @value_mask
         @value_mask = new_mask
@@ -67,26 +67,26 @@ class Tile
   end
 
   def cost_to_place(val)
-		cost = 0
-		cur_mask = @value_mask
-		while cur_mask > 0 # count my options
-			cur_mask = cur_mask >> 1
-			cost = cost+1
-		end
-		neighbors do |neighbor| # count the options my neighbors would lose if I become this
-			if (1 << (val-1)) & neighbor.value_mask > 0
-				cost = cost + 1
-			end
-		end
-		return cost
+    cost = 0
+    cur_mask = @value_mask
+    while cur_mask > 0 # count my options
+      cur_mask = cur_mask >> 1
+      cost = cost+1
+    end
+    neighbors do |neighbor| # count the options my neighbors would lose if I become this
+      if (1 << (val-1)) & neighbor.value_mask > 0
+        cost = cost + 1
+      end
+    end
+    return cost
   end
 
   # this value is illegal for all other items in this row
   def yield_row
     (0..8).each do |cur_x|
       if cur_x != @x
-				cur_tile = @puzzle[@y][cur_x]
-				yield(cur_tile)
+        cur_tile = @puzzle[@y][cur_x]
+        yield(cur_tile)
       end
     end
   end
@@ -101,7 +101,7 @@ class Tile
         cur_y = top_left_y + offset_y
         if cur_x != @x && cur_y != @y # already got row/col/self
           cur_tile = @puzzle[cur_y][cur_x]
-					yield(cur_tile)
+          yield(cur_tile)
         end
       end
     end
@@ -112,49 +112,49 @@ class Tile
     (0..8).each do |cur_y|
       if cur_y != @y
         cur_tile = @puzzle[cur_y][@x]
-				yield(cur_tile)
+        yield(cur_tile)
       end
     end
   end
 
   def copy(new_puzzle)
-		t = Tile.new
+    t = Tile.new
     t.set(new_puzzle,@x,@y)
     t.set_value(@value)
     t.set_mask(@value_mask)
     t
   end
-	# create some methods for copy which don't do propogation/etc
-	def set_value(v)
-		@value = v
-	end
-	def set_mask(mask)
-		@value_mask = mask
-	end
+  # create some methods for copy which don't do propogation/etc
+  def set_value(v)
+    @value = v
+  end
+  def set_mask(mask)
+    @value_mask = mask
+  end
 end
 
 
 def panic(p)
   puts "panic!"
   print_puzz(p)
-	success = true
-	p.each do |row|
-		row.each do |tile|
-			success = success && tile.valid?
-		end
-	end
-	if success
-		puts "success!!!!"
-	else
-		puts "error!!!!!"
-	end
+  success = true
+  p.each do |row|
+    row.each do |tile|
+      success = success && tile.valid?
+    end
+  end
+  if success
+    puts "success!!!!"
+  else
+    puts "error!!!!!"
+  end
   exit
 end
 
 def success(p)
-	puts "success"
-	print_puzz(p)
-	exit
+  puts "success"
+  print_puzz(p)
+  exit
 end
 
 # copy all values of "from" to "to"
@@ -202,33 +202,33 @@ def solve(puzz)
         if cur_val & 1 > 0
           cur_cost = tile.cost_to_place(cur_val)
           candidate_values << [tile,cur_val,cur_cost]
-					# puts "cost of #{cur_val} => (#{tile.x},#{tile.y}) is #{cur_cost}"
+          # puts "cost of #{cur_val} => (#{tile.x},#{tile.y}) is #{cur_cost}"
         end
         cur_val = cur_val+1
         cur_mask = cur_mask >> 1
       end
     end
   end
-	# puts candidate_values.inspect
-	if candidate_values.length == 0
-		success(puzz)
-	else
-		while candidate_values.length > 0 do
-			# puts "choose a good candidate to set the value on"
-			candidate_values.sort! {|one,two| one[2] <=> two[2] }
-			# puts candidate_values.inspect
-			next_puz = copy_puzz(puzz)
-			chosen = candidate_values.shift
-			begin
-				next_puz[chosen.y][chosen.x].value = chosen[1]
-				solve(next_puzz)
-			rescue
-				puts "learned that #{chosen[1]} does NOT go at #{chosen[0].x},#{chosen[0].y}"
-				puzz[chosen[0].y][chosen[0].x].mask(~(1<<(chosen[1]-1)))
-			end
-		end
-	end
-	panic(puzz)
+  # puts candidate_values.inspect
+  if candidate_values.length == 0
+    success(puzz)
+  else
+    while candidate_values.length > 0 do
+      # puts "choose a good candidate to set the value on"
+      candidate_values.sort! {|one,two| one[2] <=> two[2] }
+      # puts candidate_values.inspect
+      next_puz = copy_puzz(puzz)
+      chosen = candidate_values.shift
+      begin
+        next_puz[chosen.y][chosen.x].value = chosen[1]
+        solve(next_puzz)
+      rescue
+        puts "learned that #{chosen[1]} does NOT go at #{chosen[0].x},#{chosen[0].y}"
+        puzz[chosen[0].y][chosen[0].x].mask(~(1<<(chosen[1]-1)))
+      end
+    end
+  end
+  panic(puzz)
 end
 
 
@@ -260,11 +260,11 @@ File.open('puzzle.sudoku').each_line do |s|
     puts "#{n} at #{x},#{y}"
     if n > 0 && n < 10
       puts "#{n} at (#{x},#{y}) => #{n}"
-			begin
-				puzzle[y][x].value = n
-			rescue
-				panic(puzzle)
-			end
+      begin
+        puzzle[y][x].value = n
+      rescue
+        panic(puzzle)
+      end
     end
     x = x + 1
   end
