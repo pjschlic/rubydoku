f1f1e641a80e5ff9eca7701e9041b5460154de97
Peter Schlichting <pjschlic@gmail.com>
2012-10-03 01:44:28 -0700

    turns out we are NOT fully doing this right yet...

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -14,6 +14,16 @@ puzzle = []
 end
 puts "total of #{puzzle.length} rows"
 
+def panic(p)
+	p.each do |r|
+		r.each do |c|
+			print "#{c} "
+		end
+		puts ""
+	end
+	exit
+end
+
 # print the puzzle's status stuff
 def print_puzz(puzz)
 	puzz.each do |row|
@@ -24,12 +34,29 @@ def print_puzz(puzz)
 	end
 end
 
+# If the puzzle is UNDER specified, start to choose some random valid values
+def solve_under_specified_puzz(puzz)
+	puts "under-specified puzzle?"
+	puzz.each_index do |y|
+		row = puzz[y]
+		row.each_index do |x|
+			val = Math.log2(puzz[y][x])
+			if val > 0 && val < 9 && val != val.ceil
+				# find highest allowable value
+				puts "Putting #{val.floor.to_i + 1} (allow #{puzz[y][x]}) at (#{x},#{y})"
+				place_val(puzz,x,y,(1<<(val.floor.to_i)))
+			end
+		end
+	end
+end
+
 # this value is illegal for all other items in this row
 def set_row(puz, x, y, val)
 	(0..8).each do |cur_x|
 		puz[y][cur_x] = puz[y][cur_x]&(~val) unless cur_x == x
 		if puz[y][cur_x] == 0
-			puts "ERROR!!!! ILLEGAL BOARD"
+			puts "ERROR!!!! ILLEGAL BOARD syncing row at #{cur_x},#{y}"
+			panic(puz)
 		end
 	end
 end
@@ -39,7 +66,8 @@ def set_col(puz, x, y, val)
 	(0..8).each do |cur_y|
 		puz[cur_y][x] = puz[cur_y][x]&(~val) unless cur_y == y
 		if puz[cur_y][x] == 0
-			puts "ERROR!!!! ILLEGAL BOARD"
+			puts "ERROR!!!! ILLEGAL BOARD syncing column at #{x},#{cur_y}"
+			panic(puz)
 		end
 	end
 end
@@ -54,10 +82,23 @@ def set_section(puz, x, y, val)
 			cur_y = top_left_y + offset_y
 			puz[cur_y][cur_x] = puz[cur_y][cur_x]&(~val) unless (cur_y == y && cur_x == x)
 			if puz[cur_y][cur_x] == 0
-				puts "ERROR!!!! ILLEGAL BOARD"
+				puts "ERROR!!!! ILLEGAL BOARD syncing block at #{cur_x},#{cur_y}"
+				panic(puz)
 			end
 		end
 	end
+end
+
+# take care of all logic to place a value at a place
+def place_val(puz,x,y,val)
+	if puz[y][x]&val == 0
+		puts "ERROR INVALID PUZZLE: #{val} being placed at (#{x},#{y}) which is #{puz[y][x]}"
+		panic(puzzle)
+	end
+	puz[y][x] = val
+	set_row(puz,x,y,val)
+	set_col(puz,x,y,val)
+	set_section(puz,x,y,val)
 end
 
 y = 0
@@ -70,17 +111,16 @@ File.open('puzzle.sudoku').each_line do |s|
 		if n > 0 && n < 10
 			val = 1 << (n-1)
 			puts "#{n} at (#{x},#{y}) => #{val}"
-			if puzzle[y][x]&val == 0
-				puts "ERROR INVALID PUZZLE #{val} being placed at (#{x},#{y}) which is #{puzzle[y][x]}"
-			end
-			puzzle[y][x] = val
-			set_row(puzzle,x,y,val)
-			set_col(puzzle,x,y,val)
-			set_section(puzzle,x,y,val)
+			place_val(puzzle,x,y,val)
 		end
 		x = x + 1
 	end
 	y = y + 1
 end
+
+print_puzz(puzzle)
+
+solve_under_specified_puzz(puzzle)
+
 
 print_puzz(puzzle)
